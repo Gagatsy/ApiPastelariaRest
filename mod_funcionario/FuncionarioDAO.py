f3929a601b98d1d1dd9baebb0272c94c8a7fe704
Alexsander Vieira <alexsander_kapiva@hotmail.com>
2023-09-12 13:44:05 -0300

    Feito todos os CRUD

--- mod_funcionario/FuncionarioDAO.py
+++ mod_funcionario/FuncionarioDAO.py
@@ -74,3 +74,37 @@ def post_funcionario(corpo: Funcionario):
         return {"erro": str(e)}, 400
     finally:
         session.close()
+        
+@router.put("/funcionario/{id}", tags=["Funcionário"])
+def put_funcionario(id: int, corpo: Funcionario):
+    try:
+        session = db.Session()
+        dados = session.query(FuncionarioDB).filter(FuncionarioDB.id_funcionario == id).one()
+        dados.nome = corpo.nome
+        dados.cpf = corpo.cpf
+        dados.telefone = corpo.telefone
+        dados.senha = corpo.senha
+        dados.matricula = corpo.matricula
+        dados.grupo = corpo.grupo
+        session.add(dados)
+        session.commit()
+        return {"id": dados.id_funcionario}, 200
+    except Exception as e:
+        session.rollback()
+        return {"erro": str(e)}, 400
+    finally:
+        session.close()
+
+@router.delete("/funcionario/{id}", tags=["Funcionário"])
+def delete_funcionario(id: int):
+    try:
+        session = db.Session()
+        dados = session.query(FuncionarioDB).filter(FuncionarioDB.id_funcionario == id).one()
+        session.delete(dados)
+        session.commit()
+        return {"id": dados.id_funcionario}, 200
+    except Exception as e:
+        session.rollback()
+        return {"erro": str(e)}, 400
+    finally:
+        session.close()
---
+++ mod_produto/ProdutoModel.py
@@ -0,0 +1,20 @@
+import db
+from sqlalchemy import Column, VARCHAR, CHAR, Integer
+
+
+# ORM
+
+class ProdutoDB(db.Base):
+    __tablename__ = 'tb_produto'
+    id_produto = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    nome = Column(VARCHAR(100), nullable=False)
+    descricao = Column(VARCHAR(100), unique=True, nullable=False)
+    foto = Column(CHAR(11), nullable=False)
+    valor_unitario = Column(Integer, nullable=False)
+    
+    def __init__(self, id_produto, nome, descricao, foto, valor_unitario):
+        self.id_produto = id_produto
+        self.nome = nome
+        self.descricao = descricao
+        self.foto = foto
+        self.valor_unitario = valor_unitario
